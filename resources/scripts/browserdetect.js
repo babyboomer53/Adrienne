625e918f0001cc675912f73ba0a465ca7ce6df44
babyboomer53 <edgar.cole@san.rr.com>
2018-10-01 20:55:21 -0700

    Just trying to clean things up

---
+++ resources/scripts/browserdetect.js
@@ -0,0 +1,48 @@
+// Get IE or Edge browser version
+var version = detectIE();
+
+if (version === false) {
+    var x = document.getElementsByClassName("inner");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.add("inset-shadow");
+    }
+} else {
+    var x = document.getElementsByClassName("inner");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove("inset-shadow");
+    }
+
+}
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+
+    var msie = ua.indexOf("MSIE ");
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+    }
+
+    var trident = ua.indexOf("Trident/");
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf("rv:");
+        return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+    }
+
+    var edge = ua.indexOf("Edge/");
+    if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+    }
+    // other browser
+    return false;
+}
