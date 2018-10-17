6867c839d73cdcf47007951d8bce4286380b9c42
babyboomer53 <edgar.cole@san.rr.com>
2018-10-17 12:07:26 -0700

    Changed the filename extensions of the audio files from ogg to oga

--- resources/scripts/browserdetect.js
+++ resources/scripts/browserdetect.js
@@ -1,54 +1,54 @@
 // Get IE or Edge browser version
 
-var version = detectIE();
-var x = document.getElementsByClassName("inner");
-var y = document.getElementsByClassName("clock-container");
+    var version = detectIE();
+    var x = document.getElementsByClassName("inner");
+    var y = document.getElementsByClassName("clock-container");
 
-if (version === false) {
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].classList.add("inset-shadow");
-    }
-    var i;
-    for (i = 0; i < x.length; i++) {
-        y[i].classList.add("inset-shadow");
-    }
-} else {
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].classList.add("alt-shadow");
-    }
-    var i;
-    for (i = 0; i < x.length; i++) {
-        y[i].classList.add("alt-shadow");
-    }
-}
-
-/**
- * detect IE
- * returns version of IE or false, if browser is not Internet Explorer
- */
-
-function detectIE() {
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf("MSIE ");
-    if (msie > 0) {
-        // IE 10 or older => return version number
-        return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+    if (version === false) {
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.add("inset-shadow");
+        }
+        var i;
+        for (i = 0; i < x.length; i++) {
+            y[i].classList.add("inset-shadow");
+        }
+    } else {
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.add("alt-shadow");
+        }
+        var i;
+        for (i = 0; i < x.length; i++) {
+            y[i].classList.add("alt-shadow");
+        }
     }
 
-    var trident = ua.indexOf("Trident/");
-    if (trident > 0) {
-        // IE 11 => return version number
-        var rv = ua.indexOf("rv:");
-        return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
-    }
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
 
-    var edge = ua.indexOf("Edge/");
-    if (edge > 0) {
-        // Edge (IE 12+) => return version number
-        return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)), 10);
+        }
+
+        var trident = ua.indexOf("Trident/");
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf("rv:");
+            return parseInt(ua.substring(rv + 3, ua.indexOf(".", rv)), 10);
+        }
+
+        var edge = ua.indexOf("Edge/");
+        if (edge > 0) {
+            // Edge (IE 12+) => return version number
+            return parseInt(ua.substring(edge + 5, ua.indexOf(".", edge)), 10);
+        }
+        // other browser
+        return false;
     }
-    // other browser
-    return false;
-}
--- resources/scripts/datetime.js
+++ resources/scripts/datetime.js
@@ -1,67 +1,69 @@
-function date_time(id) {
-    date = new Date();
-    year = date.getFullYear();
-    month = date.getMonth();
-    months = new Array(
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "Jully",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    );
-    d = date.getDate();
-    day = date.getDay();
-    days = new Array(
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    );
-    h = date.getHours();
-    if (h >= 12) {
-        period = 'PM'
-    } else {
-        period = 'AM'
-    };
-    h = h % 12;
-    if (h==0) {h = 12}
-    //if (h < 10) {
-    //    h = "0" + h;
-    //};
-    m = date.getMinutes();
-    if (m < 10) {
-        m = "0" + m;
+    function date_time(id) {
+        date = new Date();
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array(
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "Jully",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        );
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array(
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        );
+        h = date.getHours();
+        if (h >= 12) {
+            period = 'PM'
+        } else {
+            period = 'AM'
+        };
+        h = h % 12;
+        if (h == 0) {
+            h = 12
+        }
+        //if (h < 10) {
+        //    h = "0" + h;
+        //};
+        m = date.getMinutes();
+        if (m < 10) {
+            m = "0" + m;
+        }
+        s = date.getSeconds();
+        if (s < 10) {
+            s = "0" + s;
+        }
+        result =
+            "" +
+            days[day] +
+            " " +
+            months[month] +
+            " " +
+            d +
+            " " +
+            year +
+            " @ " +
+            h +
+            ":" +
+            m +
+            " " +
+            period;
+        document.getElementById(id).innerHTML = result;
+        setTimeout('date_time("' + id + '");', "1000");
+        return true;
     }
-    s = date.getSeconds();
-    if (s < 10) {
-        s = "0" + s;
-    }
-    result =
-        "" +
-        days[day] +
-        " " +
-        months[month] +
-        " " +
-        d +
-        " " +
-        year +
-        " @ " +
-        h +
-        ":" +
-        m +
-        " " +
-        period;
-    document.getElementById(id).innerHTML = result;
-    setTimeout('date_time("' + id + '");', "1000");
-    return true;
-}
